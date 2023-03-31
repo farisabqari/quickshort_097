8fde10e060674e59ccf97f9b3bf288b47186c71a
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-03-31 16:24:46 +0800

    memasukan void q_short

--- quickshort/quickshort.cpp
+++ quickshort/quickshort.cpp
@@ -40,3 +40,60 @@ void swap(int x, int y)
     arr[y] = temp;
 }
 
+void q_short(int low, int high)
+{
+    int pivot, i, j;
+    if (low > high)                                             // Langkah Algoritma No.1
+        return;
+
+    // Partition the list into two parts:
+    // One containing elements less that or equal to pivot
+    // Outher containing elements greater than pivot
+
+    pivot = arr[low];                                           // Langkah Algoritma No.2
+
+    i = low + 1;                                                // Langkah Algoritma No.3
+    j = high;                                                   // Langkah Algoritma No.4
+
+
+    while (i <= j)                                              // Langkah Algoritma No.10
+    {
+        // Search for an element greater than pivot
+        while ((arr[i] <= pivot) && (i <= high))                // Langkah Algoritma No.5
+        {
+            i++;                                                // Langkah Algoritma No.6
+            cmp_count++;
+        }
+        cmp_count++;
+
+        // Search for an element less than or equal to pivot
+        while ((arr[j] > pivot) && (j >= low))                  // Langkah Algoritma No.7
+        {
+            j--;                                                // Langkah Algoritma No.8
+            cmp_count++;
+        }
+        cmp_count++;
+
+        // If the greater element is on the left of the element
+        if (i < j)                                              // Langkah Algoritma No.9
+        {
+            // Swap the element at index i with the element at index j
+            swap(i, j);
+            mov_count++;
+        }
+    }
+    // J now containt the index of the last element in the sorted list
+    if (low < j);                                               // Langkah Algoritma No.11
+    {
+        // Move the pivot to its correct position in the list
+        swap(low, j);
+        mov_count++;
+    }
+    // Sort the list on the left of pivot using quick sort
+    q_short(low, j - 1);                                        // Langkah Algoritma No.12
+
+    // Sort the list on the right of pivot using quick sort
+    q_short(j + 1, high);                                       // Langkah Algoritma No.13
+
+}
+
