90482e376d2f98e1456502f54ee8f88044e91c48
farisabqari <127277279+farisabqari@users.noreply.github.com>
2023-03-31 16:17:50 +0800

    masukan int

---
+++ quickshort/quickshort.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[20];            // Array of integers to hold values
+int cmp_count = 0;      // Number of comparison
+int mov_count = 0;      // Number of data movements
+int n;
+
